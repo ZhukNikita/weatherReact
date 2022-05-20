359317e2f260b9316cf2d933bbd73ee422fadf95
ZhukNikita <48828173+ZhukNikita@users.noreply.github.com>
2022-05-20 13:34:52 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,14 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter , Route , Routes} from 'react-router-dom';
-import store from './redux/redux.js'
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App store ={props.store}/>
+    <App/>
   </React.StrictMode>
 );
 reportWebVitals();
